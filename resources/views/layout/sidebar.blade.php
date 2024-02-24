378c942fee2881a08c2d86d606aff778d3991a6a
M. Toni <112196332+Toni1709@users.noreply.github.com>
2024-02-24 18:34:38 +0700

    Update sidebar.blade.php

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -11,13 +11,7 @@
     </div>
     <div class="sidebar-body">
         <ul class="nav">
-            <li class="nav-item nav-category">Main</li>
-            <li class="nav-item">
-                <a href="dashboard.html" class="nav-link">
-                    <i class="link-icon" data-feather="box"></i>
-                    <span class="link-title">Dashboard</span>
-                </a>
-            </li>
+            <li class="nav-item nav-category">Main</li>           
             <li class="nav-item">
                 <a href="{{ route('manajemenMobil') }}" class="nav-link">
                     <i class="link-icon" data-feather="box"></i>
